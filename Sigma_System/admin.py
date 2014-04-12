bda6ea979dd21666f7e6a1b978e7e5c32ad3663f
Cristian <kandia88@gmail.com>
2014-04-12 08:54:32 -0400

    Validando datos de alta_usuario

--- Sigma_System/admin.py
+++ Sigma_System/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from Sigma_System.models import *
+
+admin.site.register(Proyecto)
+admin.site.register(Fase)
+admin.site.register(Permiso)
+admin.site.register(Rol)
+admin.site.register(Usuario)
+admin.site.register(UsuarioRol)
+admin.site.register(Atributo)
+admin.site.register(TipoDeItem)
+admin.site.register(AtribTipoDeItem)
+admin.site.register(Archivo)
+admin.site.register(LBase)
+admin.site.register(Item)
+admin.site.register(ItemAtributosTipoI)
+admin.site.register(TipoModificado)
+admin.site.register(Historial)
+admin.site.register(Solicitud)
+admin.site.register(Comite)
+admin.site.register(HistorialLineabase)
--- Sigma_System/views.py
+++ Sigma_System/views.py
@@ -39,15 +39,44 @@ def alta_usuario(request):
     if request.method == 'POST':
         form = FormAltaUsuario(request.POST, request.FILES)
         if form.is_valid():
-            usuario = User.objects.create(username=form.cleaned_data['nombre_usuario'],
-                                          first_name=form.cleaned_data['nombre'],
-                                          last_name=form.cleaned_data['apellido'],
-                                          email=form.cleaned_data['email'],
-                                          password=make_password(form.cleaned_data['contrasenha']))
-            Usuario.objects.create(user=usuario, ci=form.cleaned_data['ci'],
-                                           direccion=form.cleaned_data['direccion'],
-                                           tel=form.cleaned_data['tel'],
-                                           estado=True)
+            print ("entro en valid")
+            print(form.cleaned_data['nombre_usuario'])
+            user = User.objects.filter(username=form.cleaned_data['nombre_usuario'])
+            print (user)
+            if user is not None:
+                print ("entro en None 1")
+                user = User.objects.filter(email=form.cleaned_data['email'])
+                print(form.cleaned_data['email'])
+                print (user)
+                if user:
+                    print ("entro en None 2")
+                    user = Usuario.objects.filter(ci=form.cleaned_data['ci'])
+                    print(form.cleaned_data['ci'])
+                    print (user)
+                    if user is None:
+                        print ("entro en None 3")
+                        '''usuario = User.objects.create(username=form.cleaned_data['nombre_usuario'],
+                                                      first_name=form.cleaned_data['nombre'],
+                                                      last_name=form.cleaned_data['apellido'],
+                                                      email=form.cleaned_data['email'],
+                                                      password=make_password(form.cleaned_data['contrasenha']))
+                        Usuario.objects.create(user=usuario, ci=form.cleaned_data['ci'],
+                                                       direccion=form.cleaned_data['direccion'],
+                                                       tel=form.cleaned_data['tel'],
+                                                       estado=True)'''
+                    else:
+                        print "entro aca1"
+                        form = FormAltaUsuario()
+                        return render(request, 'Alta Usuario.html', {'form': form, 'alerta': 'Ya existe este ci'})
+                else:
+                    print "entro aca2"
+                    form = FormAltaUsuario()
+                    return render(request, 'Alta Usuario.html', {'form': form, 'alerta': 'Ya existe este e-mail'})
+            else:
+                print "entro aca3"
+                form = FormAltaUsuario()
+                return render(request, 'Alta Usuario.html', {'form': form, 'alerta': 'Ya existe este username'})
+        else:
             return HttpResponseRedirect('/ss/adm_u/')
     else:
         form = FormAltaUsuario()
