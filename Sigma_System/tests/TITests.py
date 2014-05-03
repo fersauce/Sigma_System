95a8bcb5481f4b2f22723c030da0cd676150d70d
fersauce <carlifer.fernando@gmail.com>
2014-05-03 03:47:50 -0400

    Tests (faltan)

---
+++ Sigma_System/tests/TITests.py
@@ -0,0 +1,10 @@
+from django.test.testcases import TestCase
+
+
+class TITesting(TestCase):
+    """
+    Clase que realiza los tests de Tipo de Items.
+
+    @author: Fernando Saucedo
+    """
+    pass
