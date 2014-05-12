e1d0a8c322f5c59b992557cdf2c5d19adb9597e2
fersauce <carlifer.fernando@gmail.com>
2014-05-11 22:55:09 -0400

    Wsgi que si funciona

--- CRF_Project/wsgi.py
+++ CRF_Project/wsgi.py
@@ -7,8 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+'''import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CRF_Project.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+application = get_wsgi_application()'''
+
+import os
+import sys
+from unipath import Path
+p = Path(__file__)
+sys.path = [p.ancestor(1)] + sys.path
+os.environ['DJANGO_SETTINGS_MODULE'] = 'CRF_Project.settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
