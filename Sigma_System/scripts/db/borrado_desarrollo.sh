e05a0cc049fb7aeec08d6a572e5bb86e6a556f44
fersauce <carlifer.fernando@gmail.com>
2014-05-14 18:16:43 -0400

    Script de borrado de des modificado.

--- Sigma_System/scripts/db/borrado_desarrollo.sh
+++ Sigma_System/scripts/db/borrado_desarrollo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-psql -h localhost -t -d ss_des -U sigmasystem -c "DROP SCHEMA public CASCADE;" > droptables
+psql -h localhost -t -d ss_des -U sigmasystem -c "DROP SCHEMA public CASCADE
+CREATE SCHEMA public" > droptables
 
 psql -h localhost -d ss_des -U sigmasystem -f droptables
