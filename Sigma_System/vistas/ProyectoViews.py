7739d57623a6340d5645075d479f977c1113bc76
fersauce <carlifer.fernando@gmail.com>
2014-05-15 22:21:40 -0400

    Proyecto casi completo, falta ver detalles del modal nada mas al asignar usuarios. También está la alta del proyecto, y la CREACIÓN del comité de cambios.

--- Sigma_System/vistas/ProyectoViews.py
+++ Sigma_System/vistas/ProyectoViews.py
@@ -1,9 +1,17 @@
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.context_processors import csrf
+from django.core.serializers import serialize
+from django.core.urlresolvers import reverse
+from django.db.models.query import QuerySet
 from django.shortcuts import render, render_to_response
 from django.http import *
 from django.template import RequestContext
+import simplejson
+import sys
 from Sigma_System.forms import BusquedaProyectoForm, AltaProyectoForm
-from Sigma_System.models import Proyecto, Usuario, Fase
+from Sigma_System.models import Proyecto, Usuario, Fase, UsuariosXProyecto, \
+    UsuarioRol, Rol, Comite, UsuarioPorComite
 import datetime, time
 
 
@@ -12,9 +20,10 @@ def administrar_proyecto(request):
     Vista para acceder a la administracion de proyectos.
     """
     proyectos = Proyecto.objects.all().order_by('-nombre')
+    usuario = Usuario.objects.all()
     return render(request, 'administrarproyectos.html',
                   {'proyectos': proyectos,
-                   'vacio': 'No se encuentran proyectos registrados',
+                   'usuarios': usuario,
                    'form': BusquedaProyectoForm()})
 
 
@@ -29,13 +38,15 @@ def alta_proyecto(request):
     @return: AdministrarProyecto.html, pagina en la cual se trabaja con los
     proyectos.
     """
+    rol = Rol.objects.get(pk=5)
+    lideres = UsuarioRol.objects.filter(rol__pk=rol.pk)
     if request.method == 'POST':
         proyecto = Proyecto.objects.filter(
             nombre=request.POST['nombreProyecto'])
         if proyecto.__len__() == 0:
             fecha = datetime.datetime.now()
             try:
-                print request.POST['fechaInicio']
+                tl = Usuario.objects.get(pk=request.POST['lider'])
                 nuevoProyecto = Proyecto(
                     nombre=request.POST['nombreProyecto'],
                     fechaCreacion=fecha,
@@ -43,14 +54,39 @@ def alta_proyecto(request):
                     complejidad=0,
                     costo=0,
                     estado='Pendiente',
-                    nroFases=0,
+                    nroFases=request.POST['nroFases'],
                     nroMiembros=1,
                     duracion=0,
                     fechaInicio=datetime.datetime.now(),
                     fechaFinalizacion=datetime.datetime.now() + datetime.timedelta(
-                        days=10)
+                        days=10),
+                    lider=tl
                 )
                 nuevoProyecto.save()
+                lider = UsuariosXProyecto(
+                    proyecto=nuevoProyecto,
+                    usuario=tl,
+                    activo=True,
+                    lider=True
+                )
+                lider.save()
+                usuarios = Usuario.objects.all().exclude(pk=tl.pk)
+                for usuario in usuarios:
+                    UsuariosXProyecto.objects.create(
+                        proyecto=nuevoProyecto,
+                        usuario=usuario
+                    )
+                comite = Comite.objects.create(
+                    obs='Comite de cambios del proyecto ' +
+                        nuevoProyecto.nombre + '',
+                    nro_integ=1,
+                    fecha_creacion=datetime.datetime.now(),
+                    proy=Proyecto
+                )
+                usuariosComite = UsuarioPorComite.objects.create(
+                    comite=comite,
+                    usuario=tl
+                )
                 messages.success(request, 'Proyecto ' + str(
                     nuevoProyecto.nombre) + ' creado con exito')
             except Exception as error:
@@ -62,9 +98,9 @@ def alta_proyecto(request):
             messages.error(request,
                            'No se pudo crear el proyecto: El nombre ya '
                            'existe en el sistema.')
-            return render(request, 'proyectoalta.html')
+            return render(request, 'proyectoalta.html', {'lideres': lideres})
 
-    return render_to_response('proyectoalta.html',
+    return render_to_response('proyectoalta.html', {'lideres': lideres},
                               context_instance=RequestContext(request))
 
 
@@ -155,3 +191,119 @@ def buscar_proyecto(request):
                    'vacio': 'No se encuentran proyectos con ese '
                             'patron de busqueda',
                    'form': BusquedaProyectoForm()})
+
+
+def administrarProyectosAsociados(request):
+    usuario = Usuario.objects.all()
+    proyectos = Proyecto.objects.all()
+    c = {}
+    c.update(csrf(request))
+    return render(request, 'des_admin_proyectos.html',
+                  {'proyectos': proyectos, 'usuarios': usuario})
+
+
+def asignarUsuarioProyecto(request, idProyect):
+    """
+    Vista que asigna los usuarios a un proyecto determinado.
+
+    @type request: django.http.HttpRequest.
+    @param request: Contiene la informacion sobre la solicitud de la pagina
+    que lo llamo.
+
+    @type idProyect: Unicode
+    @param idProyect: Contiene el id del proyecto que se va a asociar/desasociar
+    usuarios.
+
+    @rtype django.shortcuts.render
+    @return: AdministrarProyecto.html, pagina en la cual se trabaja con los
+    proyectos.
+    """
+    if request.is_ajax():
+        print 'LLamada de ajax'
+        enviar = []
+        for u in UsuariosXProyecto.objects.filter(
+                proyecto=Proyecto.objects.get(pk=idProyect)).exclude(
+                lider=True):
+            enviar.append(
+                {'id': u.usuario.pk, 'nombre': u.usuario.user.first_name,
+                 'apellido': u.usuario.user.last_name, 'activo': u.activo})
+        return HttpResponse(simplejson.dumps(enviar),
+                            mimetype='application/json')
+    usuariosProyect = request.GET.getlist('usuarioAsig')
+    proyecto = Proyecto.objects.get(pk=idProyect)
+    usuarios = UsuariosXProyecto.objects.filter(proyecto=proyecto).exclude(
+        lider=True)
+    bandera = False
+    for u in usuarios:
+        if str(u.usuario.pk) in usuariosProyect:
+            if not u.activo:
+                try:
+                    u.activo = True
+                    u.save()
+                    proyecto.nroMiembros += 1
+                    proyecto.save()
+                    messages.success(request,
+                                     'El usuario ' +
+                                     u.usuario.user.first_name
+                                     + ' ha sido asignado al proyecto ' +
+                                     proyecto.nombre)
+                    bandera = False
+                except Exception:
+                    messages.error(request, 'Ha ocurrido un erro interno, favor'
+                                            ' contacte al administrador')
+        else:
+            if u.activo:
+                try:
+                    u.activo = False
+                    u.save()
+                    proyecto.nroMiembros -= 1
+                    proyecto.save()
+                    messages.success(request,
+                                     'El usuario ' +
+                                     u.usuario.user.first_name
+                                     + ' ha sido desasignado del proyecto ' +
+                                     proyecto.nombre)
+                    bandera = False
+                except Exception:
+                    messages.error(request, 'Ha ocurrido un erro interno, favor'
+                                            ' contacte al administrador')
+    if bandera:
+        messages.info(request, 'No se han realizados asignaciones/'
+                               'desasignaciones en el proyecto ' +
+                               proyecto.nombre)
+    return HttpResponseRedirect(reverse('sigma:adm_proy'))
+
+
+def iniciarProyecto(request, idProyect):
+    """
+    Vista que asigna los usuarios a un proyecto determinado.
+
+    @type request: django.http.HttpRequest.
+    @param request: Contiene la informacion sobre la solicitud de la pagina
+    que lo llamo.
+
+    @type idProyect: Unicode
+    @param idProyect: Contiene el id del proyecto que se va a iniciar.
+
+    @rtype django.shortcuts.render
+    @return: AdministrarProyecto.html, pagina en la cual se trabaja con los
+    proyectos.
+    """
+    proyecto = Proyecto.objects.get(pk=idProyect)
+    fases = Fase.objects.filter(proyecto=proyecto)
+    if fases.__len__() != proyecto.nroFases:
+        messages.error(request, 'No puede iniciar el proyecto, aun no se han'
+                                'definido todas las fases')
+        return HttpResponse(reverse('sigma:adm_proy'))
+    try:
+        proyecto.estado = 'Iniciado'
+        proyecto.fechaInicio = datetime.datetime.now()
+        proyecto.save()
+        messages.success(request,
+                         'El proyecto ' + proyecto.nombre + ' ha sido iniciado, '
+                                                            'el lider ya puede '
+                                                            'trabajar por el')
+    except Exception:
+        messages.error(request, 'Ha ocurrido un error interno')
+        sys.exc_info()
+    return HttpResponseRedirect(reverse('sigma:adm_proy'))
