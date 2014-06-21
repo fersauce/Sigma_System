cce1e00d88bb79eb34c3592bc2484f07a81e6f60
fersauce <carlifer.fernando@gmail.com>
2014-06-21 08:40:01 -0400

    Cambios en script de población de desarrollo

--- Sigma_System/scripts/db/poblado_produccion.sh
+++ Sigma_System/scripts/db/poblado_produccion.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pg_restore -i -h localhost -p 5432 -U sigmasystem -d ss_des -v "poblado_tablas_produccion.backup"
+pg_restore -i -h localhost -p 5432 -U sigmasystem -d ss_produccion -v "poblado_tablas_produccion.backup"
